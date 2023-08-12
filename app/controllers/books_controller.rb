4d21b42fbafbcc388eb8c17e0d9c37da906914ae
EC2 Default User <ec2-user@ip-172-31-1-221.ap-northeast-1.compute.internal>
2023-08-12 11:34:00 +0000

    [Finish] Book_view,action作成完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
 
   def index
     @book = Book.new
+    @books = Book.all
   end
   
   def create
@@ -19,11 +20,24 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
   
   private
   
   def book_params
-    params.require(:book).permit(:title, :body, :image)
+    params.require(:book).permit(:title, :body)
   end
 end
